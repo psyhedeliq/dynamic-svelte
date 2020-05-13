cfe2cd4672c32e363299fce24011bed1519354de
Cornel <filipcornelcristian@gmail.com>
2020-05-13 08:25:33 +0300

    implement <svelte:window>, <svelte:body> and <svelte:head>

--- src/App.svelte
+++ src/App.svelte
@@ -3,6 +3,12 @@
   import CartItem from "./CartItem.svelte";
   import FamilyNode from "./FamilyNode.svelte";
 
+  let y;
+
+  $: console.log(y);
+
+  let currentTitle = "My App";
+
   let familyStructure = [
     {
       isParent: true,
@@ -27,15 +33,40 @@
       renderedComponent = { cmp: Product, title: "Test Product", id: "p1" };
     }
   };
+
+  const switchTitle = () => {
+    currentTitle = "Amazing Coding Machine";
+  };
 </script>
 
-<button on:click={toggle}>Toggle Display</button>
+<style>
+  /* we add this to test the scrollY below */
+  div {
+    height: 3000px;
+  }
+</style>
 
-<svelte:component
-  this={renderedComponent.cmp}
-  title={renderedComponent.title}
-  id={renderedComponent.id} />
+<svelte:window bind:scrollY={y} />
 
-{#each familyStructure as familyMember}
-  <FamilyNode member={familyMember} />
-{/each}
+<!-- the svelte:body is for interacting with events on the body document -->
+<svelte:body on:mouseenter />
+
+<!-- in the svelte:head we can add any elements that would normally be in the head seaction of the ccomponent -->
+<svelte:head>
+  <title>{currentTitle}</title>
+</svelte:head>
+
+<button on:click={switchTitle}>Switch Title</button>
+
+<div>
+  <button on:click={toggle}>Toggle Display</button>
+
+  <svelte:component
+    this={renderedComponent.cmp}
+    title={renderedComponent.title}
+    id={renderedComponent.id} />
+
+  {#each familyStructure as familyMember}
+    <FamilyNode member={familyMember} />
+  {/each}
+</div>
